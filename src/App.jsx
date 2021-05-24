e0d8dd9aa864a6d030f3854080ca9f674e7aeefd
Florence <florence.nway@hotmail.com>
2021-05-24 22:56:14 +0100

    component init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const app = props => {
+    return (
+        <div className='App'>
+            Hello World
+        </div>
+    );
+};
+
+app.propTypes = {
+    
+};
+
+export default app;
