3b9bf073b036fa18745697c37db5e1b3474d8690
Florence <florence.nway@hotmail.com>
2021-06-27 20:55:03 +0100

    wrap with router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-ReactDom.render(<App/>, document.getElementById('root')); // (what to render, where to render)
+ReactDom.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root')); // (what to render, where to render)
